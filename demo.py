7f9992bf07e153002b83d7463ea4a7a22150e952
SErAphLi <seraphlivery@gmail.com>
2019-09-09 00:09:32 +0800

    Use vidgear to record

--- demo.py
+++ demo.py
@@ -1,72 +1,104 @@
+import os
+
+os.environ["KIVY_NO_CONSOLELOG"] = "1"
+
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
 from kivy.properties import BooleanProperty
+from kivy.config import Config
 
-import mss
-import mss.tools
-import cv2
-import numpy as np
+from vidgear.gears import ScreenGear
+from vidgear.gears import WriteGear
 
-video_name = 'Output.mp4'
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+import datetime
 
+VIDEO_NAME = 'Output.mp4'
 
-def numpy_flip(im):
-    """ Most efficient Numpy version as of now. """
-    frame = np.array(im, dtype=np.uint8)
-    frame = np.flip(frame[:, :, :3], 2)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    return frame
+output_params = {"-vcodec": "libx264", "-crf": 0,
+                 "-preset": "fast"}
 
 
 class MyApp(App):
     state = BooleanProperty(False)
 
     def build(self):
-        self.sct = mss.mss()
-        self.video = None
+        self.interval = 5
+        self.stream = None
+        self.writer = None
+
         self.title = 'Desktop Time Lapse'
         layout = BoxLayout(orientation='vertical')
-        self.label = Label(text='State: False')
-        self.btn = Button(text='Start')
-        layout.add_widget(self.label)
-        layout.add_widget(self.btn)
+
+        h_layout = BoxLayout(orientation='horizontal')
+        h_layout.add_widget(Label(text='State:'))
+        self.state_label = Label(text='Idle')
+        h_layout.add_widget(self.state_label)
+        h_layout.add_widget(Label(text='Duration:'))
+        self.rt_label = Label(text='0')
+        h_layout.add_widget(self.rt_label)
+        layout.add_widget(h_layout)
+
+        h_layout = BoxLayout(orientation='horizontal')
+        _h_layout = BoxLayout(orientation='horizontal')
+        _h_layout.add_widget(Label(text='VTime:'))
+        self.vt_label = Label(text='0')
+        _h_layout.add_widget(self.vt_label)
+        h_layout.add_widget(_h_layout)
+        self.btn = Button(text='Start Record')
+        h_layout.add_widget(self.btn)
+        layout.add_widget(h_layout)
         self.btn.bind(on_press=self.click_btn)
-        self.event = Clock.schedule_interval(self.record_image, 0.2)
+        self.event = Clock.schedule_interval(self.record_image, 0.1)
         return layout
 
     def click_btn(self, instance):
-        self.state = not self.state
-        self.label.text = 'State: {}'.format(self.state)
-        if self.btn.text == 'Start':
-            self.btn.text = 'Stop'
+        if not self.state:
+            self.state_label.text = 'Record'
+            self.btn.text = 'Stop Record'
+            self.start_time = datetime.datetime.now()
+            self.stream = ScreenGear(
+                monitor=1, **{'THREADED_QUEUE_MODE': False}).start()
+            self.writer = WriteGear(output_filename=VIDEO_NAME,
+                                    compression_mode=True,
+                                    **output_params)
+            self.count = 0
+            self.count_frames = 0
         else:
-            self.btn.text = 'Start'
-
-    def save_video(self):
-        if self.video is not None:
-            self.video.release()
-            self.video = None
+            self.state_label.text = 'Idle'
+            self.btn.text = 'Start Record'
+            self.stream.stop()
+            self.stream = None
+            self.writer.close()
+            self.writer = None
+            self.start_time = None
+        self.state = not self.state
 
     def on_stop(self):
-        self.save_video()
-
-    def on_state(self, instance, value):
-        self.save_video()
+        if self.writer:
+            self.writer.close()
+        if self.stream:
+            self.stream.stop()
 
     def record_image(self, dt):
         if not self.state:
             return
-        sct_img = self.sct.grab(self.sct.monitors[1])
-        if self.video is None:
-            self.video = cv2.VideoWriter(
-                video_name, fourcc, 30, (sct_img.width, sct_img.height))
-        self.video.write(numpy_flip(sct_img))
+        if self.count % int(self.interval / 0.1) == 0:
+            frame = self.stream.read()
+            self.writer.write(frame)
+            self.count_frames += 1
+        self.count += 1
+        d_time = str(datetime.datetime.now() - self.start_time)
+        self.rt_label.text = d_time[:d_time.find('.')]
+        v_time = str(datetime.timedelta(seconds=int(self.count_frames // 25)))
+        self.vt_label.text = v_time
 
 
 if __name__ == '__main__':
+    Config.set('graphics', 'width', '400')
+    Config.set('graphics', 'height', '100')
+    Config.set('graphics', 'resizable', '0')
+    Config.write()
     MyApp().run()
